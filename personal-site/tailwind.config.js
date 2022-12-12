d4e916b1e7ca353e2fa22ffd932d13c72756fee9
Andres Villalobos <andres.villalobos@datagrove.com>
2022-12-12 11:28:02 -0600

    tailwind config (incomp)

--- personal-site/tailwind.config.js
+++ personal-site/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './pages/**/*.html',
-      './pages/**/*.astro'
+      './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
     ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
   }
   
