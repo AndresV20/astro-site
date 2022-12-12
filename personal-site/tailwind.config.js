614d1914ffd6fb32b0a6e330dab2bcc97d7fdbb0
Andres Villalobos <andres.villalobos@datagrove.com>
2022-12-12 12:50:35 -0600

    adding the navbar with tailwind, not working

--- personal-site/tailwind.config.js
+++ personal-site/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     content: [
       './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
     ],
+    mode: 'jit',
+    darkMode:'class',
     theme: {
       extend: {},
     },
